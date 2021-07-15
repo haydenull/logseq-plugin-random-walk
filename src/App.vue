c52500764fd707b28016d1a39dc50ebf0a0df580
haydenull <brooklyn1984@qq.com>
2021-07-15 20:37:10 +0800

    feat: :tada: initial commit

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="calendar-wrap"
+       @click="_onClickOutside"
+  >
+    <div class="calendar-inner" v-if="ready">
+      <div>sdfjasdjfha</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      
+    }
+  },
+  mounted () {
+    logseq.App.onThemeModeChanged((label) => {
+      this.opts[`is-dark`] = label === 'dark'
+    })
+
+    logseq.once('ui:visible:changed', ({ visible }) => {
+      visible && (this.ready = true)
+    })
+  },
+  methods: {
+    async _updateCalendarInMonth () {
+      // const journals = await this._getCurrentRepoRangeJournals()
+      // this.journals = journals.reduce((ac, it) => {
+      //   const k = it[`journal-day`].toString()
+      //   ac[k] = it
+      //   return ac
+      // }, {})
+      // console.debug('[query journals]', journals)
+      // const dates = journals.map(it => {
+      //   const d = dayjs(it[`journal-day`].toString())
+      //   if (d.isValid()) {
+      //     return d.toDate()
+      //   }
+      // })
+      // this.opts.attributes = [
+      //   {
+      //     dot: true,
+      //     dates,
+      //   },
+      // ]
+    },
+    _onToPage (e) {
+      // this.mDate = e
+    },
+    async _getCurrentRepoRangeJournals () {
+      // const { month, year } = this.mDate
+      // const my = year + (month < 10 ? '0' : '') + month
+      // let ret
+      // try {
+      //   ret = await logseq.DB.datascriptQuery(`
+      //     [:find (pull ?p [*])
+      //      :where
+      //      [?b :block/page ?p]
+      //      [?p :block/journal? true]
+      //      [?p :block/journal-day ?d]
+      //      [(>= ?d ${my}01)] [(<= ?d ${my}31)]]
+      //   `)
+      // } catch (e) {
+      //   console.error(e)
+      // }
+      // return (ret || []).flat()
+    },
+    _onClickOutside ({ target }) {
+      const inner = target.closest('.calendar-inner')
+      !inner && logseq.hideMainUI()
+    },
+    _onDaySelect ({ id }) {
+      // this.date = id
+      // const k = id.replaceAll('-', '')
+      // if (this.journals.hasOwnProperty(k)) {
+      //   id = this.journals[k][`original-name`]
+      // }
+      // logseq.App.pushState('page', { name: id })
+      // logseq.hideMainUI()
+    },
+  },
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,64 @@
+import '@logseq/libs'
+import { createApp } from 'vue'
+import App from './App.vue'
+// import './index.css'
+
+console.log('[faiz:] === random walk', logseq)
+
+/**
+ * user model
+ */
+function createModel () {
+  return {
+    openCalendar () {
+      console.log('[faiz:] === openCalendar click', logseq)
+      logseq.showMainUI()
+    },
+  }
+}
+
+/**
+ * app entry
+ */
+function main () {
+  logseq.setMainUIInlineStyle({
+    position: 'fixed',
+    zIndex: 11,
+  })
+
+  const key = logseq.baseInfo.id
+
+  logseq.provideStyle(`
+    @import url("https://at.alicdn.com/t/font_2409735_r7em724douf.css");
+
+    div[data-injected-ui=open-calendar-${key}] {
+      display: flex;
+      align-items: center;
+      opacity: .55;
+      font-weight: 500;
+      position: relative;
+      top: 0px;
+    }
+
+    div[data-injected-ui=open-calendar-${key}]:hover {
+      opacity: .9;
+    }
+  `)
+
+  // external btns
+  logseq.provideUI({
+    key: 'hayden-random-walk',
+    path: '#search',
+    template: `
+      <a data-on-click="openCalendar" style="opacity: .6; display: inline-flex; padding-left: 3px; position: relative; bottom: -3px;">
+        <i class="iconfont icon-Calendaralt2"></i>
+      </a>
+    `,
+  })
+
+  // main UI
+  createApp(App).use(VCalendar, {}).mount('#app')
+}
+
+// bootstrap
+logseq.ready(createModel()).then(main)
